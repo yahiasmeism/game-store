34f157df0b5b73e58d5295dfefabda7371de7b76
Eithan Smith <yahia.smeism@sappro-bilisim.com>
2024-09-22 16:24:02 +0300

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 function App() {
   return <Grid
     templateAreas={{ base: `"nav" "main"`, lg: `"nav nav""aside main"` }}>
-    <GridItem area={"nav"} bg="coral" >
+    <GridItem area={"nav"}>
       <NavBar />
     </GridItem>
     <Show above="lg">
